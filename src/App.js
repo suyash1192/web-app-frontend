d457e086c68c01602ae8b1bcbad3eea28d76a2dd
Suyash Agrawal <suyash1192agr@gmail.com>
2022-03-10 16:27:12 +0530

    Added functionality to fetch data from local API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const [productList, setProductList] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products/')
+    .then(response => response.json())
+    .then(data => setProductList(data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center'}}>
+      Products:
+      {productList?.map((product)=> (
+        <li key={product.id} style={{listStyleType: 'none'}}>
+          {product.title}
+        </li>
+      ))}
     </div>
+
   );
 }
 
